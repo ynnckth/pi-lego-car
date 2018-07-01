032f6fc1549f32ef7cb8dbc65243b660144c8736
ynnckth <yannickstreit@hotmail.com>
2018-07-01 18:13:29 +0200

    add example for controlling servo with pigpio

--- server/command-listener.js
+++ server/command-listener.js
@@ -1,4 +1,6 @@
 
+const Gpio = require('pigpio').Gpio;
+
 const THROTTLE = 'THROTTLE';
 const STEER_LEFT = 'LEFT';
 const STEER_RIGHT = 'RIGHT';
@@ -8,11 +10,24 @@ const STOP_THROTTLE = 'STOP_THROTTLE';
 // TODO: add GPIO pins on raspberry pi
 const ACTION_EVENT = 'action';
 
-
 class CommandListener {
 
     constructor(io) {
         this.io = io;
+        this.motor = new Gpio(10, {mode: Gpio.OUTPUT});
+        this.pulseWidth= 1000;
+        this.increment = 100;
+
+        setInterval(function () {
+            this.motor.servoWrite(pulseWidth);
+
+            this.pulseWidth += this.increment;
+            if (this.pulseWidth >= 2000) {
+                this.increment = -100;
+            } else if (this.pulseWidth <= 1000) {
+                this.increment = 100;
+            }
+        }, 1000);
     }
 
     startListening() {
