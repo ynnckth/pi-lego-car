73632a75c390f26306e35828e3e58fa9a1ec429f
ynnckth <yannickstreit@hotmail.com>
2018-07-29 13:38:10 +0200

    use servoWrite instead of pwmWrite for steering motor

--- server/src/steering-unit.js
+++ server/src/steering-unit.js
@@ -5,11 +5,6 @@ const CENTER = 'center';
 
 const SERVO_SIGNAL_PIN = 10;    // GPIO10, Pin #19
 
-// TODO: fix and adapt values to the servo motor
-const PWM_DUTY_CYLE_LEFT = 70;
-const PWM_DUTY_CYLE_CENTER = 160;
-const PWM_DUTY_CYLE_RIGHT = 250;
-
 
 /**
  * The steering unit handles the left/right movement of the rover.
@@ -21,23 +16,20 @@ class SteeringUnit {
     }
 
     /**
-     * The servo motor is controlled with pulse with modulation (PWM).
-     *
-     * Gpio.pwmWrite(dutyCycle)
-     * dutyCycle: integer between 0 (off) and 255 (fully on)
-     * Uses DMA to control and schedule the pulse lengths and duty cycle
-     * See https://github.com/fivdi/pigpio/blob/master/doc/gpio.md#pwmwritedutycycle
+     * Controls the servo motor.
+     * See https://github.com/fivdi/pigpio/blob/master/doc/gpio.md#servowritepulsewidth
      */
     steer(direction) {
+	console.log("steering " + direction);
         switch (direction) {
             case LEFT:
-                this.servoSignal.pwmWrite(PWM_DUTY_CYLE_LEFT);
+                this.servoSignal.servoWrite(2000);
                 break;
             case CENTER:
-                this.servoSignal.pwmWrite(PWM_DUTY_CYLE_CENTER);
+                this.servoSignal.servoWrite(1500);
                 break;
             case RIGHT:
-                this.servoSignal.pwmWrite(PWM_DUTY_CYLE_RIGHT);
+                this.servoSignal.servoWrite(1000);
                 break;
         }
     }
